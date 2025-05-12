84ec67f7e46dcabec7ca9835e019d39b60eaf795
Shabet <pakjunyoung@nate.com>
2025-05-12 20:29:08 +0900

    초기 프로젝트 (수정)

--- src/main/java/hello/springtx/SpringtxApplication.java
+++ src/main/java/hello/springtx/SpringtxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JdbcProjectApplication {
+public class SpringtxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JdbcProjectApplication.class, args);
+        SpringApplication.run(SpringtxApplication.class, args);
     }
 
 }
